09e6c92006f7f757260ea288809af6c38e06ae56
Sofia Yfantidou <syfantid@csd.auth.gr>
2017-11-30 11:39:25 +0100

    Updates mapStreams benchmarking

--- src/DatabaseSystemArchitecture.java
+++ src/DatabaseSystemArchitecture.java
@@ -1,29 +1,30 @@
 import java.io.*;
 import java.nio.MappedByteBuffer;
-import java.util.ArrayList;
 import java.util.Random;
 
 public class DatabaseSystemArchitecture {
     /* Number of open streams */
-    public static int k = 10;
+    private static int k = 5;
     /* Input and Output Streams; need to be initialized in main function */
-    public static OutStream[] out = new OutStream[k];
-    public static InStream[] in = new InStream[k];
+    static OutStream[] out = new OutStream[k];
+    static InStream[] in = new InStream[k];
     /* Variables for timing */
-    public static int elementSizeInBytes = 4;
-    public static long startReadTime;
-    public static long startWriteTime;
-    public static long endReadTime;
-    public static long endWriteTime;
-    public static long writeTime;
-    public static long readTime;
-    public static long totalTime;
+    static int elementSizeInBytes = 4;
+    static long startReadTime;
+    static long startWriteTime;
+    static long endReadTime;
+    static long endWriteTime;
+    static long writeTime;
+    static long readTime;
+    static long totalTime;
     /* Variable for random integer generation */
-    public static Random rand = new Random();
+    static Random rand = new Random();
     /* Number of records per file */
-    public static int N = 50000;
-    /* Number of buffer blocks */
-    public static int B = 20;
+    static int N = 50000;
+    /* Size of a buffer block in records */
+    static int B = 100;
+    // TODO: 30/11/2017 Before we had declared B as the number of buffers in memory; and M as the size of each memory
+    // buffer but I think it was wrong.
 
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
@@ -35,33 +36,33 @@ public class DatabaseSystemArchitecture {
 
         /* METHOD 1: SYSTEM FUNCTIONS */
         System.out.println("METHOD 1:");
-        systemStreams("testSystem");
+        systemStreams();
 
         /* METHOD 2: BUFFERED READER AND WRITER */
         System.out.println("METHOD 2:");
-        bufferedStreams("testBuffered");
+        bufferedStreams();
 
         /* METHOD 3: BUFFER READ PER BLOCK */
         System.out.println("METHOD 3:");
-        blockStreams("testBlock");
+        blockStreams();
 
         /* METHOD 4: MAPPING */
-        /*System.out.println("METHOD 4:");
-        mapStreams("testMapping");*/
+        System.out.println("METHOD 4:");
+        mapStreams();
     }
 
-    static String createFilename(String filename, int i) {
+    private static String createFilename(String filename, int i) {
         return filename + i + ".data";
     }
 
-    static void systemStreams(String filename) throws IOException {
+    private static void systemStreams() throws IOException {
         DataOutputStream[] ods = new DataOutputStream[k];
         DataInputStream[] ds = new DataInputStream[k];
 
         /* Write data to files */
         startWriteTime = System.currentTimeMillis();
         for(int i = 0; i < k; i++) {
-            ods[i] = out[i].systemCreate(createFilename(filename,i));
+            ods[i] = out[i].systemCreate(createFilename("testSystem",i));
         }
         /* For each number (i), write the number to j files */
         for(int i = 0; i < N; i++) {
@@ -82,7 +83,7 @@ public class DatabaseSystemArchitecture {
         startReadTime = System.currentTimeMillis();
         for(int i = 0; i < k; i++) {
             try {
-                ds[i] = in[i].systemOpen(createFilename(filename,i));
+                ds[i] = in[i].systemOpen(createFilename("testSystem",i));
             } catch (FileNotFoundException e) {
                 System.out.println("File not found!");
                 return;
@@ -108,14 +109,14 @@ public class DatabaseSystemArchitecture {
         System.out.println("Total Time: " + totalTime);
     }
 
-    static void bufferedStreams(String filename) throws IOException {
+    private static void bufferedStreams() throws IOException {
         DataOutputStream[] ods = new DataOutputStream[k];
         DataInputStream[] ds = new DataInputStream[k];
 
         /* Write data to files */
         startWriteTime = System.currentTimeMillis();
         for(int i = 0; i < k; i++) {
-            ods[i] = out[i].bufferedCreate(createFilename(filename,i));
+            ods[i] = out[i].bufferedCreate(createFilename("testBuffered",i));
         }
         /* For each number (i), write the number to j files */
         for(int i = 0; i < N; i++) {
@@ -137,7 +138,7 @@ public class DatabaseSystemArchitecture {
         startReadTime = System.currentTimeMillis();
         for(int i = 0; i < k; i++) {
             try {
-                ds[i] = in[i].bufferedOpen(createFilename(filename,i));
+                ds[i] = in[i].bufferedOpen(createFilename("testBuffered",i));
             } catch (FileNotFoundException e) {
                 System.out.println("File not found!");
                 return;
@@ -164,14 +165,14 @@ public class DatabaseSystemArchitecture {
         System.out.println("Total Time: " + totalTime);
     }
 
-    static void blockStreams(String filename) throws IOException, ClassNotFoundException {
+    private static void blockStreams() throws IOException, ClassNotFoundException {
         ObjectOutputStream[] oos = new ObjectOutputStream[k];
         ObjectInputStream[] ois = new ObjectInputStream[k];
 
         /* Write data to files */
         startWriteTime = System.currentTimeMillis();
         for(int i = 0; i < k; i++) {
-            oos[i] = out[i].blockCreate(createFilename(filename,i));
+            oos[i] = out[i].blockCreate(createFilename("testBlock",i));
         }
 
         int[] buffer = new int[B];
@@ -205,7 +206,7 @@ public class DatabaseSystemArchitecture {
         startReadTime = System.currentTimeMillis();
         for(int i = 0; i < k; i++) {
             try {
-                ois[i] = in[i].blockOpen(createFilename(filename,i));
+                ois[i] = in[i].blockOpen(createFilename("testBlock",i));
             } catch (FileNotFoundException e) {
                 System.out.println("File not found!");
                 return;
@@ -236,30 +237,78 @@ public class DatabaseSystemArchitecture {
         System.out.println("Total Time: " + totalTime);
     }
 
-//    static void mapStreams(String filename) throws IOException {
-//         Write data to file
-//        int[] buffer = {1,2,3};
-//        int[] buffer2 = {4,5,6};
-//        int[] buffer3 = {7,8,9};
-//        int[] buffer4 = {10,11,12};
-//        ChannelObjects channelObjects = out.channelCreate(filename, M);
-//        MappedByteBuffer map = channelObjects.getMap();
-//        out.write(map, buffer);
-//        out.write(map, buffer2);
-//        out.write(map, buffer3);
-//        out.write(map, buffer4);
-//        out.close(channelObjects);
-//
-//         Open file and read data
-//        channelObjects = in.channelOpen(filename, M);
-//        map = channelObjects.getMap();
-//        while(in.eof(map)) {
-//            int[] buf = in.read(map, B);
-//            for(int i = 0 ; i < buf.length ; i++) {
-//                System.out.println(buf[i]);
-//            }
-//            System.out.println("**********");
-//        }
-//        in.close(channelObjects);
-//    }
+    private static void mapStreams() throws IOException {
+         /*Write data to file*/
+        ChannelObjects[] channelObjects = new ChannelObjects[k];
+        MappedByteBuffer map[] = new MappedByteBuffer[k];
+        /* Write data to files */
+        startWriteTime = System.currentTimeMillis();
+        for(int i = 0; i < k; i++) {
+            channelObjects[i] = out[i].channelCreate(createFilename("testMapping",i), N);
+            map[i] = channelObjects[i].getMap();
+        }
+
+        int[] buffer = new int[B]; // A single buffer containing B elements
+        // The number of times we will need to refill the buffer will be size of file / number of elements in each buffer
+        int bufferRefills = (int)Math.ceil(N/B);
+        int counter = 0; //to check if N integers are already written in the file
+
+        /*refill the buffer array bufferRefills times using B elements and then write it to k files*/
+        outerloop:
+        for(int i = 0; i < bufferRefills; i++) {
+            for(int j = 0; j < B; j++) {
+                buffer[j] = rand.nextInt(10);
+                counter++;
+                if(counter >= N) {
+                    break outerloop;
+                }
+            }
+
+            for(int j = 0; j < k; j++) {
+                /*System.out.println(j);*/
+                out[j].write(map[j], buffer);
+            }
+        }
+
+        /* Close all files */
+        for(int i = 0; i < k; i++) {
+            out[i].close(channelObjects[i]);
+        }
+        endWriteTime = System.currentTimeMillis();
+        writeTime = endWriteTime - startWriteTime;
+        System.out.println("Write Time: " + writeTime);
+
+        /* Open files and read data */
+        startReadTime = System.currentTimeMillis();
+        for(int i = 0; i < k; i++) {
+            try {
+                channelObjects[i] = in[i].channelOpen(createFilename("testMapping",i),B);
+                map[i] = channelObjects[i].getMap();
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found!");
+                return;
+            }
+        }
+
+        for(int i = 0; i < k; i++) {
+            while (in[i].eof(map[i])) {
+                int[] buf = in[i].read(map[i], B);
+                /* Uncomment this block if you want the result to be printed on screen */
+//                for (int j = 0; j < buf.length; j++) {
+//                    System.out.println(buf[j]);
+//                }
+//                System.out.println("**********");
+            }
+        }
+
+        for(int i = 0; i < k; i++) {
+            in[i].close(channelObjects[i]);
+        }
+
+        endReadTime = System.currentTimeMillis();
+        readTime = endReadTime - startReadTime;
+        System.out.println("Read Time: " + readTime);
+        totalTime = readTime + writeTime;
+        System.out.println("Total Time: " + totalTime);
+    }
   }
--- src/OutStream.java
+++ src/OutStream.java
@@ -2,53 +2,50 @@ import java.io.*;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
-public class OutStream {
+class OutStream {
 
-    public DataOutputStream systemCreate(String filename) throws FileNotFoundException {
+    DataOutputStream systemCreate(String filename) throws FileNotFoundException {
         OutputStream os = new FileOutputStream( new File(filename) );
-        DataOutputStream ds = new DataOutputStream(os);
-        return ds;
+        return new DataOutputStream(os);
     }
 
-    public DataOutputStream bufferedCreate(String filename) throws FileNotFoundException {
+    DataOutputStream bufferedCreate(String filename) throws FileNotFoundException {
         OutputStream os = new FileOutputStream( new File(filename ) );
         BufferedOutputStream bos = new BufferedOutputStream( os );
-        DataOutputStream ds = new DataOutputStream( bos );
-        return ds;
+        return new DataOutputStream( bos );
     }
 
-    public ObjectOutputStream blockCreate(String filename) throws IOException {
-        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename));
-        return out;
+    ObjectOutputStream blockCreate(String filename) throws IOException {
+        return new ObjectOutputStream(new FileOutputStream(filename));
     }
 
-    public ChannelObjects channelCreate(String filename, int M) throws IOException {
+    ChannelObjects channelCreate(String filename, int N) throws IOException {
         RandomAccessFile memoryMappedFile = new RandomAccessFile(filename, "rw");
         FileChannel fileChannel = memoryMappedFile.getChannel();
         MappedByteBuffer map = fileChannel.map(FileChannel.MapMode.READ_WRITE,
-                0,DatabaseSystemArchitecture.elementSizeInBytes * M);
+                0,DatabaseSystemArchitecture.elementSizeInBytes * N);
         return new ChannelObjects(fileChannel, map, memoryMappedFile);
     }
 
-    public void write(DataOutputStream ds, int element) throws IOException {
+    void write(DataOutputStream ds, int element) throws IOException {
         ds.writeInt(element);
 }
 
-    public void write(ObjectOutputStream oos, int[] buffer) throws IOException {
+    void write(ObjectOutputStream oos, int[] buffer) throws IOException {
         oos.writeObject(buffer);
     }
 
-    public void write(MappedByteBuffer map, int[] buffer) throws IOException {
+    void write(MappedByteBuffer map, int[] buffer) throws IOException {
         for (int element : buffer) {
             map.putInt(element);
         }
     }
 
-    public void close(OutputStream ds) throws IOException {
+    void close(OutputStream ds) throws IOException {
         ds.close();
     }
 
-    public void close(ChannelObjects channelObjects) throws IOException {
+    void close(ChannelObjects channelObjects) throws IOException {
         channelObjects.getFileChannel().close();
         channelObjects.getRandomAccessFile().close();
     }

24595af74cc28640420b6b51c414d16903b4da50
Noor Zehra <noor.zehra@skyelectric.com>
2018-01-06 19:38:36 +0100

    Added JavaDocs

--- src/ChannelObjects.java
+++ src/ChannelObjects.java
@@ -4,11 +4,21 @@ import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
+
+/**
+ * This class is used as a data structure to store file channel, mapped byte buffer and file reference together as an object
+ */
 public class ChannelObjects {
     private FileChannel fileChannel;
     private MappedByteBuffer map;
     private RandomAccessFile randomAccessFile;
 
+    /**
+     * Constructor
+     * @param fileChannel File channel
+     * @param map Mapped Byte Buffer
+     * @param randomAccessFile File object
+     */
     public ChannelObjects(FileChannel fileChannel, MappedByteBuffer map, RandomAccessFile randomAccessFile) {
         this.fileChannel = fileChannel;
         this.map = map;
--- src/Record.java
+++ src/Record.java
@@ -1,6 +1,16 @@
+/**
+ * This class provides a data structure to insert integer into priority queue with it's file reference
+ */
+
 public class Record implements Comparable<Record> {
     private int value;
     private int fileReference;
+
+    /**
+     * Constructor
+     * @param value
+     * @param fileReference
+     */
 
     public Record(int value, int fileReference)  {
         this.value = value;
@@ -15,6 +25,12 @@ public class Record implements Comparable<Record> {
     public int getFileReference() {
         return fileReference;
     }
+
+    /**
+     * Set the criteria for comparison
+     * @param other integer to compare with
+     * @return returns the difference between 2 values
+     */
 
     public int compareTo(Record other){
         return this.value - other.getValue();
